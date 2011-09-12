c5d3e97af4c8afdf2e9c6173de541bfba1348dff
Brandon Taylor <btaylordesign@gmail.com>
2011-09-11 20:57:48 -0500

    Added project files.

---
+++ config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../compiled/css"
+sass_dir = "."
+images_dir = ""
+javascripts_dir = ""
+relative_assets = true
+output_style = :expanded
